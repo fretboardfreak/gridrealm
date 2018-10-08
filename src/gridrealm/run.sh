3fb776cf744d7864083146e051a06adba2fd78e5
Curtis Sand <curtis@curtissand.com>
2018-10-08 13:55:39 -0700

    app: build docs before running the app

--- src/gridrealm/run.sh
+++ src/gridrealm/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# Run and host the flask app with gunicorn
+# build the docs into webpages
+RST_BUILD=${HOME}/code/rstbuild.sh
+${RST_BUILD} docs/
 
+# Run and host the flask app with gunicorn
 gunicorn -b 127.0.0.1:5000 main:APP
