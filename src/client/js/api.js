9acba3bbe9ddda31c5706456b3882914bdc72fd4
Curtis Sand <curtis@curtissand.com>
2018-10-22 10:52:27 -0700

    client js: add api support for the more specific random image uri's
    Add two new URI strings, two new API functions and two new,
    corresponding APIhelper functions for the new URI's.

--- src/client/js/api.js
+++ src/client/js/api.js
@@ -8,9 +8,11 @@
 
 import {build_image_tag} from "./util.js"
 
-var version = "api/version";
-var randomImage = "api/randomImage";
-export var URI = { version, randomImage }
+export var URI = {}
+URI.version = "api/version",
+URI.randomImage = "api/randomImage",
+URI.randomActionImage = "api/randomActionImage",
+URI.randomInventoryImage = "api/randomInventoryImage"
 
 export class API {
   static get_random_image(success) {
@@ -21,7 +23,7 @@ export class API {
       success: success,
       error: function (error) {console.log(error);}
     });
-  };
+  }
 
   static get_api_version(success) {
     $.ajax({
@@ -31,15 +33,50 @@ export class API {
       success: success,
       error: function (error) {console.log(error);}
     });
-  };
+  }
+
+  static get_random_action_image(success) {
+    $.ajax({
+      url: URI.randomActionImage,
+      data: '',
+      type: 'GET',
+      success: success,
+      error: function (error) {console.log(error);}
+    });
+  }
+_
+  static get_random_inventory_image(success) {
+    $.ajax({
+      url: URI.randomInventoryImage,
+      data: '',
+      type: 'GET',
+      success: success,
+      error: function (error) {console.log(error);}
+    });
+  }
 }
 
 export class APIhelpers {
   static add_random_image(tag, height) {
     if (typeof height === 'undefined') height = "auto";
     API.get_random_image(function (response){
-      tag.html(build_image_tag(response.randomImage, height));
-  });
-}
+      tag.html(build_image_tag(response.image, height));
+    });
+  }
+
+  static add_random_action_image(tag, height) {
+    if (typeof height === 'undefined') height = "auto";
+    API.get_random_action_image(function (response){
+      tag.html(build_image_tag(response.image, height));
+    });
+  }
+
+  static add_random_inventory_image(tag, height) {
+    if (typeof height === 'undefined') height = "auto";
+    API.get_random_inventory_image(function (response){
+      console.log('adding inv. img ' + response.image);
+      tag.html(build_image_tag(response.image, height));
+    });
+  }
 
 }
