775c0041d632de9f386fa46e059d0dd2e4ca6617
Curtis Sand <curtis@curtissand.com>
2018-11-02 11:36:10 -0700

    engine: implement user login/logout behaviour
    This also uses the new system messaging system to send messages to all
    clients about logins of other clients.

--- src/gridrealm/static_routes.py
+++ src/gridrealm/static_routes.py
@@ -1,6 +1,10 @@
 """Gridrealm Static Routes: Use Flask instead of webserver for static files."""
 
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import session
 
 import gridrealm as GR
 from gridrealm.config import Config
@@ -8,7 +12,33 @@ from gridrealm.config import Config
 
 def index():
     """Static route for gridrealm client."""
-    return render_template(Config().assets.client_uri)
+    # pylint cannot find the logger object on the GR.APP
+    # pylint: disable=no-member
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        # TODO: do something to make sure user is logged in
+    if 'username' in session:
+        uname = session['username']
+        GR.APP.logger.debug('User %s logged in.' % uname)
+        response = GR.APP.make_response(
+            render_template(Config().assets.client_uri))
+        response.set_cookie('username', uname)
+        GR.SYS_MSG.publish('New login by %s' % uname)
+    else:
+        GR.APP.logger.debug('New user at landing page.')
+        response = GR.APP.make_response(
+            render_template(Config().assets.landing_uri))
+    return response
+
+
+def logout():
+    """Log the user out of their session."""
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    response = GR.APP.make_response(redirect(url_for('index')))
+    # invalidate the username cookie
+    response.set_cookie('username', '')
+    return response
 
 
 def assets(asset):
@@ -37,15 +67,17 @@ def sysmsg():
 
 
 STATIC_VIEWS = [
-    ('/', 'index', index),
-    ('/_assets/<path:asset>', 'assets', assets),
-    ('/docs/', 'docs', docs),
-    ('/docs/<path:doc>', 'docs', docs),
-    ('/favicon.ico', 'favicon', favicon)
+    (('/', 'index', index), {'methods': ['GET', 'POST']}),
+    (('/_assets/<path:asset>', 'assets', assets), {}),
+    (('/docs/', 'docs', docs), {}),
+    (('/docs/<path:doc>', 'docs', docs), {}),
+    (('/favicon.ico', 'favicon', favicon), {}),
+    (('/logout', 'logout', logout), {}),
+    (('/sysmsg', 'sysmsg', sysmsg), {})
 ]
 
 
 def register_static_views(app):
     """Register the static views with the flask app."""
-    for view in STATIC_VIEWS:
-        app.add_url_rule(*view)
+    for view_opts, view_args in STATIC_VIEWS:
+        app.add_url_rule(*view_opts, **view_args)
