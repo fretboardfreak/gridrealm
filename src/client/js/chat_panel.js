5a0863996c71954443eac12cbec66e88c6f42c4d
Curtis Sand <curtis@curtissand.com>
2018-11-02 11:41:26 -0700

    client js: subscribe chat panel to system messages

--- src/client/js/chat_panel.js
+++ src/client/js/chat_panel.js
@@ -9,5 +9,11 @@
 import {APIhelpers} from "./api.js"
 
 export function load_chat_panel() {
-  APIhelpers.add_random_action_image($("#chat-panel"));
+  var event_source = new EventSource("/sysmsg");
+  event_source.onmessage = function(e) {
+    console.log(e.data);
+    $("#chat-panel").prepend('<div class="alert-info w-100 m-0 row" ' +
+                             'role="alert" style="height: 1rem;">' +
+                             e.data + '</div>');
+  };
 }
