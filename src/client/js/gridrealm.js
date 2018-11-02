e69ae56470746ea7d8df385a518cdedc528bbdea
Curtis Sand <curtis@curtissand.com>
2018-11-02 11:38:49 -0700

    client js: if logged in, init client
    The auth.login() function is called when a user is logged in and
    triggers the code that sets up the client.

--- src/client/js/gridrealm.js
+++ src/client/js/gridrealm.js
@@ -8,13 +8,18 @@
 
 import * as auth from "./auth.js";
 import * as resize from "./resize.js";
+import { get_cookie } from "./util.js";
 
 
 /* On DOM Ready*/
 $(document).ready(function(){
   console.log("initializing page.");
-  auth.init_panels();
-  resize.resize_panels();
+  if (auth.is_logged_in()) {
+    console.log("Logged in as: " + get_cookie('username'));
+    auth.login();
+  } else {
+    console.log('Not logged in.');
+  }
 });
 
 /* Resize the game panels when browser window is resized. */
