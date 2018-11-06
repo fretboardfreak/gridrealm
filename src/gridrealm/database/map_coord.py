183d210a0ee9e56d433ec71f29a0f44472451004
Curtis Sand <curtis@curtissand.com>
2018-11-05 16:03:58 -0800

    database: add model for map coordinate

---
+++ src/gridrealm/database/map_coord.py
@@ -0,0 +1,34 @@
+"""Gridrealm Map Coordinate."""
+
+
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+from gridrealm.database import Base
+
+
+class MapCoord(Base):
+    """Table to represent the map details at each coordinate."""
+
+    # pylint: disable=invalid-name
+    __tablename__ = 'map_coords'
+
+    xcoord = Column(Integer, primary_key=True)
+    ycoord = Column(Integer, primary_key=True)
+    zcoord = Column(Integer, primary_key=True)
+    tile_id = Column(Integer, ForeignKey('map_tiles.id'), nullable=False)
+    tile = relationship('MapTile', back_populates='coords')
+
+    def __init__(self, xcoord, ycoord, zcoord, tile_id):
+        """Create an instance of the MapTile database model."""
+        self.xcoord = xcoord
+        self.ycoord = ycoord
+        self.zcoord = zcoord
+        self.tile_id = tile_id
+
+    def __repr__(self):
+        """Return a representation of the MapTile database model."""
+        return '<MapCoord: X=%d, Y=%d, Z=%d>' % (
+            self.xcoord, self.ycoord, self.zcoord)
