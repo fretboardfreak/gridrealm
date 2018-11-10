1ce241d99262e8d876fb89e76820355d86b8486c
Curtis Sand <curtis@curtissand.com>
2018-11-10 12:02:20 -0800

    engine: add api core file to hold common api code

---
+++ src/gridrealm/api/core.py
@@ -0,0 +1,18 @@
+"""Random Image URI Library."""
+
+from enum import Enum
+
+
+API_VERSION = "0.1"
+
+
+class ResourceEnum(Enum):
+    """Base API Resource object for the gridrealm API."""
+
+    @classmethod
+    def add_resources(cls, api, resource_classes=None):
+        """Register the given resources on the api object."""
+        if not resource_classes:
+            resource_classes = [res.value for res in cls]
+        for res_cls in resource_classes:
+            api.add_resource(res_cls, res_cls.uri)
