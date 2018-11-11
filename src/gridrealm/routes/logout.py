52bc8e2994060ea364b8cdec0c8e5ebac0114925
Curtis Sand <curtis@curtissand.com>
2018-11-10 19:55:33 -0800

    engine: refactor logout route to use cookies library

--- src/gridrealm/routes/logout.py
+++ src/gridrealm/routes/logout.py
@@ -2,12 +2,13 @@
 
 from time import time
 
-from flask import render_template
 from flask import session
+from flask import redirect
+from flask import url_for
 
 import gridrealm as GR
-from gridrealm.config import Config
 from gridrealm.database import User
+from gridrealm.cookies import clear_cookies
 from gridrealm.routes.core import GRView
 
 
@@ -24,15 +25,6 @@ class Logout(GRView):
         # remove the username from the session if it's there
         username = session.pop('username', None)
         session.clear()
-        response = GR.APP.make_response(
-            render_template(Config().assets.landing_uri))
-        # invalidate the session cookies
-        response.set_cookie('username', '', expires=0)
-        response.set_cookie('last_login', '', expires=0)
-        response.set_cookie('xcoord', '', expires=0)
-        response.set_cookie('ycoord', '', expires=0)
-        response.set_cookie('zcoord', '', expires=0)
-
         if username not in ['', None]:
             user = User.query.filter(User.name == username).first()
             msg = "User %s has logged out" % username
@@ -45,4 +37,7 @@ class Logout(GRView):
                 GR.DBS.commit()
             GR.SYS_MSG.publish(msg)
             GR.APP.logger.debug(msg)
+
+        response = GR.APP.make_response(redirect(url_for('index')))
+        clear_cookies(response)
         return response
