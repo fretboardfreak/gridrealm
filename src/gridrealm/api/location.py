04b7f935c54c7cec3bd1c4adff93d7cbc00e3231
Curtis Sand <curtis@curtissand.com>
2018-11-11 12:58:27 -0800

    engine: update location api to use map library

--- src/gridrealm/api/location.py
+++ src/gridrealm/api/location.py
@@ -3,9 +3,9 @@
 from flask import g as flask_g
 from flask_restful import Resource
 
-import gridrealm.database as DB
 from gridrealm.auth import user_required
 from gridrealm.api.core import ResourceEnum
+from gridrealm.game_obj import map as grmap  # don't override python builtins
 
 
 class Location(Resource):
@@ -16,38 +16,11 @@ class Location(Resource):
     def get(self):
         """API endpoint to return the version of the in use API schema."""
         response = {}
-        index_map = [[(-1, 1), (0, 1), (1, 1)],
-                     [(-1, 0), (0, 0), (1, 0)],
-                     [(-1, -1), (0, -1), (1, -1)]]
-        map_assets = []
-        user = flask_g.user
-        for i, row in enumerate(index_map):
-            map_assets.append([])
-            for col in row:
-                coord = DB.MapCoord.query.filter(
-                    DB.MapCoord.xcoord == user.xcoord + col[0]).filter(
-                        DB.MapCoord.ycoord == user.ycoord + col[1]).first()
-                map_assets[i].append(coord.tile.resource.split('\n'))
-        response['minimap'] = map_assets
-        loc = DB.MapCoord.query.filter(
-            DB.MapCoord.xcoord == user.xcoord).filter(
-                DB.MapCoord.ycoord == user.ycoord).first()
-        nloc = DB.MapCoord.query.filter(
-            DB.MapCoord.xcoord == user.xcoord).filter(
-                DB.MapCoord.ycoord == user.ycoord + 1).first()
-        sloc = DB.MapCoord.query.filter(
-            DB.MapCoord.xcoord == user.xcoord).filter(
-                DB.MapCoord.ycoord == user.ycoord + 1).first()
-        wloc = DB.MapCoord.query.filter(
-            DB.MapCoord.xcoord == user.xcoord - 1).filter(
-                DB.MapCoord.ycoord == user.ycoord).first()
-        eloc = DB.MapCoord.query.filter(
-            DB.MapCoord.xcoord == user.xcoord + 1).filter(
-                DB.MapCoord.ycoord == user.ycoord).first()
-        response['movement'] = {'north': loc.tile.exitN and nloc.tile.enterS,
-                                'east': loc.tile.exitE and eloc.tile.enterW,
-                                'south': loc.tile.exitS and sloc.tile.enterN,
-                                'west': loc.tile.exitW and wloc.tile.enterE}
+        location = grmap.get_coord(xcoord=flask_g.user.xcoord,
+                                   ycoord=flask_g.user.ycoord,
+                                   zcoord=flask_g.user.zcoord)
+        response['minimap'] = grmap.get_minimap_assets(location)
+        response['movement'] = grmap.get_minimap_move_flags(location)
         return response
 
     get = user_required(get)
