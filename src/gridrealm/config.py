1e67ab09140887301a418797e75ab928363cc4f0
Curtis Sand <curtis@curtissand.com>
2019-04-29 11:12:09 -0700

    Convert software licencse to Apache v2.0

--- src/gridrealm/config.py
+++ src/gridrealm/config.py
@@ -1,10 +1,22 @@
-"""Gridrealm Configuration."""
-
+# Copyright 2019 Curtis Sand <curtissand@gmail.com>,
+#                Dennison Gaetz <djgaetz@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Originally borrowed from
 # https://github.com/fretboardfreak/netify/master/src/netify/config.py
 # Original Author: Curtis Sand
-
-# Modifications: Copyright 2018 - Curtis Sand, Dennison Gaetz
+"""Gridrealm Configuration."""
 
 import os
 from configparser import SafeConfigParser
